c3f0dc4c6ea3be9ac4817066a1283e16f8362d4f
Luiz Gustavo Banin De Sena <luizgbs1@gmail.com>
2021-03-12 09:31:22 -0300

    Padronização

--- Faculdade.sql
+++ Faculdade.sql
@@ -1,31 +1,31 @@
-create database faculdade
+CREATE DATABASE faculdade
 
 CREATE TABLE Curso
 (
-	nome_curso varchar(20) NOT NULL,
+	nome_curso VARCHAR(20) NOT NULL,
 	CONSTRAINT pkcurso PRIMARY KEY(nome_curso) 
 );
 
 CREATE TABLE Aluno
 (
-	ra int NOT NULL,
-	nome_aluno varchar(50) NOT NULL,
-	end_logradouro varchar(20),
-	end_numero varchar(10) NOT NULL,
-	end_bairro varchar(20),
-	end_cep char(8) NOT NULL,
-	end_localidade varchar(20),
-	end_uf varchar(2),
-	nome_curso varchar(20) NOT NULL,
+	ra INT NOT NULL,
+	nome_aluno VARCHAR(50) NOT NULL,
+	end_logradouro VARCHAR(20),
+	end_numero VARCHAR(10) NOT NULL,
+	end_bairro VARCHAR(20),
+	end_cep CHAR(8) NOT NULL,
+	end_localidade VARCHAR(20),
+	end_uf VARCHAR(2),
+	nome_curso VARCHAR(20) NOT NULL,
 	
 	CONSTRAINT fkAlunoNome_curso FOREIGN KEY (nome_curso) REFERENCES Curso(nome_curso),
 	CONSTRAINT pkAlunoRA PRIMARY KEY(ra) 
 );
 CREATE TABLE FoneAluno
 (
-	ra int NOT NULL,
-	numero char(11) NOT NULL,
-	tipo varchar(20) NOT NULL,
+	ra INT NOT NULL,
+	numero CHAR(11) NOT NULL,
+	tipo VARCHAR(20) NOT NULL,
 
 	CONSTRAINT fkFoneAlunoRa FOREIGN KEY(ra) REFERENCES Aluno(ra),
 	CONSTRAINT pkFoneAlunoRaNumero PRIMARY KEY(ra,numero)
@@ -33,137 +33,160 @@ CREATE TABLE FoneAluno
 
 CREATE TABLE Disciplina
 (	
-	nome_disciplina varchar(20) NOT NULL,
-	qtd_aula int NOT NULL,
-	nome_curso varchar(20) NOT NULL,
+	nome_disciplina VARCHAR(20) NOT NULL,
+	qtd_aula INT NOT NULL,
+	nome_curso VARCHAR(20) NOT NULL,
 
 	CONSTRAINT fkDisciplinaNomeCurso FOREIGN KEY(nome_curso) REFERENCES Curso(nome_curso),
 	CONSTRAINT pkDisciplinaNomeDisciplina PRIMARY KEY(nome_disciplina)
 );
 
+-- TABELA ALUNO DISCIPLINA
 CREATE TABLE Possui
 (
-	ra int NOT NULL,
-	nome_disciplina varchar(20)NOT NULL,
-	semestre int NOT NULL,
-	falta int ,
-	situacao varchar(20),
-	ano int NOT NULL,
-	nota_b1 decimal(4,2),
-	nota_b2 decimal(4,2),
-	sub decimal(4,2),
+	ra INT NOT NULL,
+	nome_disciplina VARCHAR(20)NOT NULL,
+	semestre INT NOT NULL,
+	falta INT ,
+	situacao VARCHAR(20),
+	ano INT NOT NULL,
+	nota_b1 DECIMAL(4,2),
+	nota_b2 DECIMAL(4,2),
+	sub DECIMAL(4,2),
 
 	CONSTRAINT fkPossuiRa FOREIGN KEY(ra) REFERENCES Aluno(ra),
 	CONSTRAINT fkPossuiNomeDisciplina FOREIGN KEY(nome_disciplina) REFERENCES Disciplina(nome_disciplina),
 	CONSTRAINT pkPossuiRaNomeDisciplina PRIMARY KEY(ra,nome_disciplina,semestre)
 );
-drop TRIGGER TatualizaSituacao
+
+-- REMOÇÃO DO TRIGGER
+DROP TRIGGER TatualizaSituacao
+
+-- TRIGGER
 CREATE TRIGGER TatualizaSituacao
 ON Possui 
-AFTER Insert , Update
+FOR INSERT , UPDATE
 AS 
-begin
+BEGIN
 	DECLARE 
-		@SUB decimal(4,2),
-		@NOME varchar(20),
-		@RA int,
-		@SEMESTRE int,
-		@NOTA1 decimal(4,2) ,
-		@NOTA2 decimal(4,2),
-		@MEDIA decimal(4,2),
-		@FALTA decimal(4,2),
-		@TOTAL decimal(4,2),
-		@PORCENTAGEM decimal(4,2)
-	select @RA = ra, @NOME = nome_disciplina,@SEMESTRE = semestre ,@FAlTA = falta,@NOTA1 = nota_b1 , @NOTA2 = nota_b2, @SUB = sub
-	from INSERTED
+		-- DECLARAÇÃO DE VARIAVEIS
+		@SUB DECIMAL(4,2),
+		@NOME VARCHAR(20),
+		@RA INT,
+		@SEMESTRE INT,
+		@NOTA1 DECIMAL(4,2) ,
+		@NOTA2 DECIMAL(4,2),
+		@MEDIA DECIMAL(4,2),
+		@FALTA DECIMAL(4,2),
+		@TOTAL DECIMAL(4,2),
+		@PORCENTAGEM DECIMAL(4,2)
+
+	-- ATRIBUIÇÃO DE VARIAVEIS
+	SELECT @RA = ra, @NOME = nome_disciplina,@SEMESTRE = semestre ,@FAlTA = falta,@NOTA1 = nota_b1 , @NOTA2 = nota_b2, @SUB = sub
+	FROM INSERTED
 	
-	if @NOTA1 > @NOTA2
-	Update Possui
-	set nota_b2 = 
-	(case
-		when @SUB is NULL  then nota_b2
-		else(@SUB)
-	end 
+	-- ATUALIZAÇÃO DA MENOR NOTA PARA A NOTA DA SUBSTITUTIVA
+	IF @NOTA1 > @NOTA2 -- NOTA 2 É A MENOR
+	UPDATE Possui
+	SET nota_b2 = 
+	(CASE
+		WHEN @SUB is NULL  THEN nota_b2
+		ELSE(@SUB)
+	END 
 	) 
-	if @NOTA1< @NOTA2
-	Update Possui
-	set nota_b1 = 
-	(case
-		when @SUB is NULL  then nota_b1
-		else(@SUB)
-	end 
+	IF @NOTA1< @NOTA2 -- NOTA 1 É A MENOR
+	UPDATE Possui
+	SET nota_b1 = 
+	(CASE
+		WHEN @SUB is NULL  THEN nota_b1
+		ELSE(@SUB)
+	END 
 	)
-	set @MEDIA=(@NOTA1+@NOTA2)/2
 
-	Select @TOTAL = qtd_aula from Disciplina where nome_disciplina = @NOME
-	set @PORCENTAGEM = (@FALTA/@TOTAL)
+	-- CALCULA MEDIA
+	SET @MEDIA=(@NOTA1+@NOTA2)/2
 
-	Update Possui 
-	set situacao = 
-	(case
-		when @PORCENTAGEM > 0.250 then 'Reprovado por falta' 
-		when @MEDIA > 6.00 then 'Aprovado'
-		when @MEDIA < 5.99 then  'Reprovado por nota' 
-		when @PORCENTAGEM < 0.250 then 'Aprovado'
+	-- RESGATA A QUANTIDADE TOTAL DE AULAS DA DISCIPLINA
+	SELECT @TOTAL = qtd_aula 
+	FROM Disciplina 
+	WHERE nome_disciplina = @NOME
+
+	-- CALCULA A PORCENTAGEM DE FALTAS DO ALUNO NA DISCIPLINA
+	SET @PORCENTAGEM = (@FALTA/@TOTAL)
+
+	-- ATUALIZA A SITUAÇÃO DO ALUNO DEPENDENDO DE SUAS NOTAS E FALTAS
+	UPDATE Possui 
+	SET situacao = 
+	(CASE
+		WHEN @PORCENTAGEM > 0.250 THEN 'Reprovado por falta' 
+		WHEN @MEDIA > 6.00 THEN 'Aprovado'
+		WHEN @MEDIA < 5.99 THEN  'Reprovado por nota' 
+		WHEN @PORCENTAGEM < 0.250 THEN 'Aprovado'
 		
-	end 
-	) 
-	where ra = @RA and nome_disciplina = @NOME  and semestre = @SEMESTRE
-end;
+	END 
+	)-- CONDIÇÃO PARA ATUALIZAR OS DADOS DO ALUNO QUE ESTA INSERINDO/ALTERANDO 
+	WHERE ra = @RA AND nome_disciplina = @NOME  AND semestre = @SEMESTRE
+END;
 
-insert into Possui(ra,nome_disciplina,ano,semestre,falta,nota_b1,nota_b2)
-values(1,'Redes de Comput',2020,1,0,9,8)
+--INSERÇÃO DE DISCIPLINA ALUNO
+INSERT INTO Possui(ra,nome_disciplina,ano,semestre,falta,nota_b1,nota_b2)
+VALUES(2,'Programação linear',2020,1,0,2,5)
 
---serve para inserir depois da inserção
-update Possui
-set sub = 6
-where ra = 2 AND semestre = 1 AND nome_disciplina = 'Programacao linear'
+--IMPRESSÕES
+SELECT * FROM Disciplina
+SELECT * FROM Possui
 
-update Possui
-set falta = 10
-where ra = 1 AND semestre = 1 AND nome_disciplina = 'Redes de Comput'
+--SERVE PARA INSERIR DEPOIS DA INSERÇÃO
+UPDATE Possui
+SET sub = 6
+WHERE ra = 2 AND semestre = 1 AND nome_disciplina = 'Programacao linear'
 
-update Possui
-set nota_b1 = 6
-where ra = 1 AND nome_disciplina = 'Estrutura de Dados'
+UPDATE Possui
+SET falta = 10
+WHERE ra = 1 AND semestre = 1 AND nome_disciplina = 'Redes de Comput'
 
-insert into Curso
-values('ADS')
+UPDATE Possui
+SET nota_b1 = 6
+WHERE ra = 1 AND nome_disciplina = 'Estrutura de Dados'
 
-insert into Aluno
-values(2,'Luiz Sena','rua Sebastião','20','centro','15000852','Matão','sp','ADS')
+-- INSERÇÕES DE DADOS NO BD
+INSERT INTO Curso
+VALUES('Sistema de inf')
 
-insert into FoneAluno
-values(2,'1633846633','Fixo')
+INSERT INTO Aluno
+VALUES(1,'Isabela','rua tal','100','centro','14000852','Araraquara','sp','Sistema de inf')
 
-insert into Disciplina
-values('Matematica Discreta',30,'ADS')
+INSERT INTO FoneAluno
+VALUES(1,'1699456633','Celular')
+
+INSERT INTO Disciplina
+VALUES('Programação BD',30,'Sistema de inf')
 
 
-/*a)Quais são alunos de uma determinada disciplina ministrada no ano de 2020, com suas notas. Você definirá a disciplina.*/
-Select l.nome_aluno, p.nota_b1,p.nota_b2
-from Aluno l ,Disciplina d  join Possui p
-ON d.nome_disciplina = 'Estrutura de Dados' AND d.nome_disciplina = p.nome_disciplina
-where l.ra = p.ra and p.ano = 2020
+/*A)QUAIS SÃO ALUNOS DE UMA DETERMINADA DISCIPLINA MINISTRADA NO ANO DE 2020, COM SUAS NOTAS. VOCÊ DEFINIRÁ A DISCIPLINA.*/
+SELECT l.nome_aluno, p.nota_b1,p.nota_b2
+FROM Aluno l ,Disciplina d  JOIN Possui p
+ON d.nome_disciplina = 'Banco de dados' AND d.nome_disciplina = p.nome_disciplina
+WHERE l.ra = p.ra AND p.ano = 2020
 
-/*b)Quais  são  as  notas  de  um  aluno  em  todas  as  disciplinas  por  ele cursadas  no  2º.  Semestre  de  2019.  
-(“BOLETIM  COM  AS INFORMAÇÕES  DAS  DISCIPLINAS  CURSADAS”).  Você  definirá  o aluno.*/
-select l.nome_aluno,p.semestre,c.nome_disciplina,p.nota_b1, p.nota_b2, Cast((p.nota_b1 + p.nota_b2)/2 as decimal(4,2)) as media 
-from Aluno l,Disciplina c  join Possui p 
+/*B)QUAIS  SÃO  AS  NOTAS  DE  UM  ALUNO  EM  TODAS  AS  DISCIPLINAS  POR  ELE CURSADAS  NO  2º.  SEMESTRE  DE  2019.  
+(“BOLETIM  COM  AS INFORMAÇÕES  DAS  DISCIPLINAS  CURSADAS”).  VOCÊ  DEFINIRÁ  O ALUNO.*/
+SELECT l.nome_aluno,p.semestre,c.nome_disciplina,p.nota_b1, p.nota_b2, CAST((p.nota_b1 + p.nota_b2)/2 AS DECIMAL(4,2)) AS media 
+FROM Aluno l,Disciplina c  JOIN Possui p 
 ON p.semestre = 2  AND p.nome_disciplina = c.nome_disciplina
-WHERE l.nome_aluno = 'Isabela' and p.ano = 2019 and p.ra = l.ra
+WHERE l.nome_aluno = 'Isabela' AND p.ano = 2019 AND p.ra = l.ra
 
-/*C)Quais são os alunos reprovados por nota (média inferior a seis) no ano de 2020 e,
-o nome das disciplinas e as médias.Você definirá o curso.*/
-select l.nome_aluno,c.nome_disciplina,Cast((p.nota_b1 + p.nota_b2)/2 as decimal(4,2)) as media
-from Aluno l ,Disciplina c join Possui p
-on p.situacao = 'Reprovado por nota' and p.nome_disciplina = c.nome_disciplina
-where c.nome_curso = 'ADS' and p.ano = 2020  and p.ra = l.ra
+/*C)QUAIS SÃO OS ALUNOS REPROVADOS POR NOTA (MÉDIA INFERIOR A SEIS) NO ANO DE 2020 E,
+O NOME DAS DISCIPLINAS E AS MÉDIAS.VOCÊ DEFINIRÁ O CURSO.*/
+SELECT l.nome_aluno,c.nome_disciplina,CAST((p.nota_b1 + p.nota_b2)/2 AS DECIMAL(4,2)) AS media
+FROM Aluno l ,Disciplina c JOIN Possui p
+ON p.situacao = 'Reprovado por nota' AND p.nome_disciplina = c.nome_disciplina
+WHERE c.nome_curso = 'ADS' AND p.ano = 2020  and p.ra = l.ra
 
-/*d)Tabela que aparece o nome e telefone dos alunos matriculados em alguma disciplina na facudade.*/
-SELECT DISTINCT l.nome_aluno,t.numero
-FROM Possui p join FoneAluno t ON t.ra = p.ra
-join  Disciplina c on c.nome_disciplina = p.nome_disciplina
-join  Aluno l ON p.ra = l.ra 
+/*D)TABELA QUE INFORMA O NOME E TELEFONE DOS ALUNOS MATRICULADOS EM ALGUMA DISCIPLINA NA FACUDADE.*/
+SELECT DISTINCT l.nome_aluno,t.numero, C.nome_disciplina
+FROM Possui p JOIN FoneAluno t ON t.ra = p.ra
+JOIN  Disciplina c ON c.nome_disciplina = p.nome_disciplina
+JOIN  Aluno l ON p.ra = l.ra 
 
  
